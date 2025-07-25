621fe167307899e646ab2dc8f4c0c8fb35ea5c93
Lucas Scholz <lucas.scholz2103@gmail.com>
2025-07-25 11:46:36 +0200

    Navbar erstellt

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,13 @@
 <template>
     <header>
-        <input placeholder="Suche...">
+        <nav class="navbar">
+      <ul class="nav-list">
+        <li class="nav-item"><a href="/">Startseite</a></li>
+        <li class="nav-item"><a href="/ausbildung/">Ausbildung</a></li>
+        <li class="nav-item"><a href="/berfarung/">Berufserfahrung</a></li>
+        <li class="nav-item"><a href="/kenntnisse/">Kenntnisse</a></li>
+      </ul>
+    </nav>
     </header>
 </template>
 
@@ -13,13 +20,46 @@
 <style scoped>
 
   header {
-    background-color: cadetblue;
-    height: 8em;
-    padding: 2em;
-    display: flex;
-    align-items: flex-end;
-    border-bottom-left-radius: 1em;
-    border-bottom-right-radius: 1em;
-  }
+  font-family: "Roboto", sans-serif;
+  color: white;
+}
+
+.navbar {
+  display: flex;
+  justify-content: center;
+  background-color: #2a2252;
+  padding: 0.75em 0;
+  border-bottom-left-radius: 1em;
+  border-bottom-right-radius: 1em;
+}
+
+.nav-list {
+  list-style: none;
+  display: flex;
+  gap: 2em;
+  margin: 0;
+  padding: 0;
+}
+
+.nav-item a {
+  color: white;
+  text-decoration: none;
+  font-weight: 600;
+  padding: 0.25em 0.5em;
+  transition: background-color 0.3s ease;
+  border-radius: 4px;
+}
+
+.nav-item a:hover {
+  background-color: #666;
+}
+
+.subtitle {
+  text-align: center;
+  padding: 0.75em 0;
+  font-size: 1.5em;
+  font-weight: 500;
+  color: #ddd;
+}
 
 </style>
